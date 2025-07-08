5058c17372c8cbbe0a1e03d31321f34009317b6b
Tech-Jobayer <khanmdjobayer99@gmail.com>
2025-07-09 05:20:53 +0600

    Update script.js

--- task_auto/script.js
+++ task_auto/script.js
@@ -1,4 +1,4 @@
-const firebaseConfig = { // Changed 'Const' to 'const'
+const firebaseConfig = {
       apiKey: "AIzaSyAC4h55aA0Zz--V5ejyndzR5WC_-9rAPio",
       authDomain: "subscribe-bot-6f9b2.firebaseapp.com",
       databaseURL: "https://subscribe-bot-6f9b2-default-rtdb.firebaseio.com",
